7c786ce20c37cca3a48c5380eea23b5fcc1c9802
linnerrion <146371998+linnerrion@users.noreply.github.com>
2025-01-10 18:42:47 +0300

    Добавил стиль кнопок и полей ввода

---
+++ src/views/Content/controllers/AtomTest.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="fw-btn fw-btn--disabled" disabled>Кнопка</button>
+    <button class="fw-btn fw-btn--primary">Кнопка</button>
+    <input class="fw-input" type="text" placeholder="Текстовое поле">
+    <input class="fw-input fw-input--disabled" type="text" placeholder="Текстовое поле" disabled> 
+</template>
